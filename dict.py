109a32439c867710d8400870462aaad792916ada
shreeyabelwalkar <shreeya.belwalkar16@gmail.com>
2023-08-25 21:45:27 +0530

    New file dict.py is added

---
+++ dict.py
@@ -0,0 +1,12 @@
+d={101:'Sharvee',102:'Shreeya',103:'Vinay',104:'Ganesh'}
+print(d)
+d[105]='Vinayak'
+d[106]='Poonam'
+print(d)
+print(d[102])
+print(d.keys())
+print(d.values())
+d[108]='Madhuri'
+print(d)
+del d[103]
+print(d)
